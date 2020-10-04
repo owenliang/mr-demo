091ebdad476f96d80abdc4ec30f9049862469e31
liangdong <liangdong@smzdm.com>
2020-10-04 16:47:11 +0800

    'update'

--- src/main/java/cc/yuerblog/Main.java
+++ src/main/java/cc/yuerblog/Main.java
@@ -1,5 +1,7 @@
 package cc.yuerblog;
 
+import cc.yuerblog.mr.AvroMR;
+import cc.yuerblog.mr.SequenceMR;
 import org.apache.hadoop.conf.Configuration;
 import cc.yuerblog.mr.RawMR;
 
@@ -8,6 +10,13 @@ import java.io.IOException;
 public class Main {
     // MapReduce官方: https://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html
     // inputformat解释：https://www.doudianyun.com/2018/11/mapreduce-inputformat/
+
+    // mapreduce核心理解：
+    // mapper输入的文件格式需要通过对应的inputformat来进行分片与解析，输入必须兼容到Key,value的方式。
+    // mapper输出没有文件格式（mr框架内置），只需要key和value可序列化即可（writeable)，比如avro对象序列化就是一种。
+    // reducer输入key和value是writeable的
+    // reducer输出的文件格式需要通过outputformat指定，其作用是兼容key,value到具体文件格式，比如输出为avro container file格式。
+
     public static void main(String []args) {
         Configuration conf = new Configuration();
 
@@ -15,6 +24,14 @@ public class Main {
             // 文本文件(带压缩)MR
             RawMR rawMR = new RawMR();
             rawMR.run(conf);
+
+            // sequencefile的MR
+            SequenceMR seqMR = new SequenceMR();
+            seqMR.run(conf);
+
+            // Avro的MR
+            AvroMR avroMR = new AvroMR();
+            avroMR.run(conf);
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ src/main/java/cc/yuerblog/mr/SequenceMR.java
@@ -0,0 +1,69 @@
+package cc.yuerblog.mr;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class SequenceMR {
+    private static class Mapper extends org.apache.hadoop.mapreduce.Mapper<Text, Text, Text, LongWritable> {
+        @Override
+        protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+            Text row = new Text(String.format("%s=%s", key.toString(), value.toString()));
+            context.write(row, new LongWritable(1));  // (内容,1次)
+        }
+    }
+
+    private static class Reducer extends org.apache.hadoop.mapreduce.Reducer<Text, LongWritable, Text, LongWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            long count = 0;
+            for (LongWritable v : values) {
+                count += v.get();
+            }
+            context.write(key, new LongWritable(count));
+        }
+    }
+
+    public void run(Configuration conf) throws Exception {
+        Path input = new Path("/sequence.txt");
+        Path output = new Path("/sequence-mr");
+
+        // 删除之前的结果
+        FileSystem.get(conf).delete(output, true);
+
+        // 创建Job
+        Job job = Job.getInstance(conf, "SequenceMR");
+        job.setJarByClass(SequenceMR.class); // mapper/reducer实现在该类内部，需要设置这个
+
+        /// 输入
+        SequenceFileInputFormat.addInputPath(job, input);   // 文件路径
+        job.setInputFormatClass(SequenceFileInputFormat.class); // 文件格式
+        job.setMapperClass(SequenceMR.Mapper.class);   // mapper实现
+        job.setMapOutputKeyClass(Text.class);   // 中间key
+        job.setMapOutputValueClass(LongWritable.class); // 中间value
+
+        // 输出
+        SequenceFileOutputFormat.setOutputPath(job, output);    // 文件路径
+        SequenceFileOutputFormat.setCompressOutput(job, true);  // 开启压缩
+        SequenceFileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);    // gzip压缩
+        SequenceFileOutputFormat.setOutputCompressionType(job, SequenceFile.CompressionType.BLOCK); // 块级压缩
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);   // 文件格式
+        job.setReducerClass(SequenceMR.Reducer.class); // reducer实现
+        job.setOutputKeyClass(Text.class);  // 结果key
+        job.setOutputValueClass(LongWritable.class);    // 结果value
+
+        // 等待任务执行，打印详情
+        job.waitForCompletion(true);
+    }
+}
